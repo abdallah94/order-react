88529dccd19d0abc61e78e2bd966c1147f8e5f0f
abdullah banishamsa <abdullah.banishamsa@exalt.ps>
2017-04-25 00:19:53 +0300

    added navItems to navbar

--- src/app/shared/dashboard/Dashboard.jsx
+++ src/app/shared/dashboard/Dashboard.jsx
@@ -5,25 +5,24 @@ import "./style.css";
 
 import React, {Component} from "react";
 import {Navbar,Nav,NavItem} from 'react-bootstrap';
+import {Link} from 'react-router';
 
 class Dashboard extends Component {
     render() {
         return (
             <Navbar inverse collapseOnSelect>
                 <Navbar.Header>
-                    <Navbar.Brand>
-                        <a href="#">React-Bootstrap</a>
+                    <Navbar.Brand>{/*TODO:ADD LOGO*/}
+                        <Link to="/"><h5>ORDERS</h5></Link>
                     </Navbar.Brand>
                     <Navbar.Toggle />
                 </Navbar.Header>
                 <Navbar.Collapse>
-                    <Nav>
-                        <NavItem eventKey={1} href="#">Link</NavItem>
-                        <NavItem eventKey={2} href="#">Link</NavItem>
-                    </Nav>
                     <Nav pullRight>
-                        <NavItem eventKey={1} href="#">Link Right</NavItem>
-                        <NavItem eventKey={2} href="#">Link Right</NavItem>
+                        <NavItem eventKey={1}><h5>Login</h5></NavItem>
+                        <NavItem eventKey={2} href="#"><h5>العربية</h5></NavItem>
+                        <NavItem eventKey={3}><h5>0595453476</h5></NavItem>
+                        <NavItem eventKey={4}><h5>CART</h5></NavItem> 
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
