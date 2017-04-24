f23ba857db1c25660411fd619956cfa5db1908b5
abdullah banishamsa <abdullah.banishamsa@exalt.ps>
2017-04-25 00:03:56 +0300

    added dashboard

--- src/app/shared/dashboard/Dashboard.jsx
+++ src/app/shared/dashboard/Dashboard.jsx
@@ -1,3 +1,34 @@
 /**
  * Created by Abdallah on 4/24/2017.
  */
+import "./style.css";
+
+import React, {Component} from "react";
+import {Navbar,Nav,NavItem} from 'react-bootstrap';
+
+class Dashboard extends Component {
+    render() {
+        return (
+            <Navbar inverse collapseOnSelect>
+                <Navbar.Header>
+                    <Navbar.Brand>
+                        <a href="#">React-Bootstrap</a>
+                    </Navbar.Brand>
+                    <Navbar.Toggle />
+                </Navbar.Header>
+                <Navbar.Collapse>
+                    <Nav>
+                        <NavItem eventKey={1} href="#">Link</NavItem>
+                        <NavItem eventKey={2} href="#">Link</NavItem>
+                    </Nav>
+                    <Nav pullRight>
+                        <NavItem eventKey={1} href="#">Link Right</NavItem>
+                        <NavItem eventKey={2} href="#">Link Right</NavItem>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        );
+    }
+}
+
+export default Dashboard;
--- src/app/shared/dashboard/DashboardContainer.js
+++ src/app/shared/dashboard/DashboardContainer.js
@@ -1,3 +1,19 @@
 /**
  * Created by Abdallah on 4/24/2017.
  */
+
+/* Components */
+import Dashboard from "./Dashboard";
+/* Modules */
+import {connect} from "react-redux";
+
+const mapStateToProps = (state) => {
+    return {}
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {}
+};
+
+let DashboardContainer = connect(mapStateToProps, mapDispatchToProps)(Dashboard);
+export {DashboardContainer};
