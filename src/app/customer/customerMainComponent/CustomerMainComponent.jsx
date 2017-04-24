1e34fd8ca22827b6a269ca0e58202097f16afb24
abdullah banishamsa <abdullah.banishamsa@exalt.ps>
2017-04-25 02:02:46 +0300

    used empty search component

--- src/app/customer/customerMainComponent/CustomerMainComponent.jsx
+++ src/app/customer/customerMainComponent/CustomerMainComponent.jsx
@@ -2,12 +2,17 @@ import "./style.css";
 import React, {Component} from "react";
 import {Grid, Row} from 'react-bootstrap';
 
+import {SearchContainer} from '../';
+
 class CustomerMainComponent extends Component {
     render() {
         return (
             <Grid fluid>
                 <Row>
                     <h1>Customer</h1>
+                </Row>
+                <Row>
+                    <SearchContainer/>
                 </Row>
             </Grid>
         );
