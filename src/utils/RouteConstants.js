ba5efc242333f0cf6d802a7bd4f4a1f9aa89d1bc
abdullah banishamsa <abdullah.banishamsa@exalt.ps>
2017-04-25 01:35:13 +0300

    added route Constants

---
+++ src/utils/RouteConstants.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Abdallah on 4/25/2017.
+ */
+const RouteConstants = {
+    ROUTE_LOGIN: "login",
+    ROUTE_APP_CUSTOMER: "customer",
+    ROUTE_APP_ADMIN: "admin",
+    ROUTE_APP_RESTAURANT: "restaurant",
+};
+
+const PathConstants = {
+    PATH_LOGIN: "/" + RouteConstants.ROUTE_LOGIN,
+    PATH_APP_CUSTOMER: "/" + RouteConstants.ROUTE_APP_CUSTOMER,
+    PATH_APP_ADMIN: "/" + RouteConstants.ROUTE_APP_ADMIN,
+    PATH_APP_RESTAURANT: "/" + RouteConstants.ROUTE_APP_RESTAURANT,
+};
+
+
+export {RouteConstants, PathConstants};
--- src/utils/index.js
+++ src/utils/index.js
@@ -3,6 +3,9 @@
  */
 import Constants from './Constants';
 import {initializeI18} from './i18-initializer';
+import {RouteConstants, PathConstants} from './RouteConstants';
 
 export {Constants};
 export {initializeI18};
+export{RouteConstants};
+export {PathConstants};
