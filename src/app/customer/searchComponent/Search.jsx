db5a096c56e8083a519d77a06ea371fdc6552824
abdullah banishamsa <abdullah.banishamsa@exalt.ps>
2017-04-28 18:25:58 +0300

    added search select

--- src/app/customer/searchComponent/Search.jsx
+++ src/app/customer/searchComponent/Search.jsx
@@ -1,12 +1,46 @@
 import "./style.css";
 import React, {Component} from "react";
+import {Form, FormGroup, Col, Row} from 'react-bootstrap';
+import Select from 'react-select';
+import i18next from 'i18next';
 
 class Search extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: 0,
+        }
+    }
+
     render() {
         return (
-            <h1>search</h1>
+            <Row>
+                <Form horizontal>
+                    <FormGroup>
+                        <Col xs={5}>
+                            <Select value={this.state.value}
+                                    multi={false}
+                                    simpleValue={true}
+                                    disabled={false}
+                                    onChange={this.handleSelectChange.bind(this)}
+                                    options={this.props.options}>
+
+                            </Select>
+                        </Col>
+                    </FormGroup>
+                </Form>
+            </Row>
         );
     }
+
+    handleSelectChange(value) {
+        console.log(value);
+        if(!value){
+            value=0;
+        }
+        this.setState({value:value});
+    }
+
 }
 
 export {Search};
--- src/app/customer/searchComponent/SearchContainer.js
+++ src/app/customer/searchComponent/SearchContainer.js
@@ -9,12 +9,25 @@ import {Constants} from '../../../utils';
 import {connect} from "react-redux";
 
 const mapStateToProps = (state) => {
-    return {}
+    return {//TODO: Add Restaurants from API
+        options: getDummyData(),
+    }
 };
 
 const mapDispatchToProps = (dispatch) => {
     return {}
 };
+
+//TODO: Delete dummy data
+function getDummyData() {
+    return [{value: 0, label: "Restaurant", disabled:true}, {value: 1, label: "MCdonalds"}, {value: 2, label: "KFC"}, {
+        value: 3,
+        label: "Mr.Pizza"
+    }, {
+        value: 4,
+        label: "Buffalo wings"
+    }];
+}
 
 let SearchContainer = connect(mapStateToProps, mapDispatchToProps)(Search);
 export {SearchContainer};
