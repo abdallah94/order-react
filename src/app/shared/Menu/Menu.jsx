261909f4272315cd83a37af0ce1ade2577d4ff5c
abdullah banishamsa <abdullah.banishamsa@exalt.ps>
2017-06-08 00:05:40 +0300

    added cart to menu

--- src/app/shared/Menu/Menu.jsx
+++ src/app/shared/Menu/Menu.jsx
@@ -7,18 +7,25 @@ import './style.css'
 
 /* Components */
 import {OrderContainer} from '../';
+import {CartContainer} from '../../customer';
 
 /*Modules*/
 import React from 'react';
+import {Row, Col} from 'react-bootstrap';
 
 export class Menu extends React.Component {
     render() {
         return (
-            <div>
-                {!!this.props.items && this.props.items.map((item) => (
-                    <OrderContainer key={item.id} {...item}/>
-                ))}
-            </div>
+            <Row>
+                <Col xs={12} md={6}>
+                    {!!this.props.items && this.props.items.map((item) => (
+                        <OrderContainer key={item.id} {...item}/>
+                    ))}
+                </Col>
+                <Col xs={12} md={6}>
+                    <CartContainer/>
+                </Col>
+            </Row>
         )
     }
 }
