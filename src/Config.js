973e019e9a64c38920da507858346da1534eba9a
abdullah banishamsa <abdullah.banishamsa@exalt.ps>
2017-07-08 19:11:47 +0300

    added config file

---
+++ src/Config.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Abdallah on 7/3/2017.
+ */
+
+const Config = (process.env.NODE_ENV === 'production') ? {//production
+        SERVER_HOST_URL: "http://192.168.10.10",//add production server url here
+        SERVER_PATH_URL: "/api/",//add production server path url here
+
+    } : {//development
+        SERVER_HOST_URL: "http://192.168.10.10",// add development server url here
+        SERVER_PATH_URL: "/api/",// add development path url here
+    };
+export {Config};
---
+++ src/utils/APIConstants.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Fujitsu on 7/8/2017.
+ */
+import {Config} from '../Config';
+export const APIConstants={
+    GET_RESTAURANTS:Config.SERVER_HOST_URL+Config.SERVER_PATH_URL+"restaurant",
+};
